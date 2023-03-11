b4ea4e5bb49bbc16ec90c0a915afaf83e6cc6f01
Jason Nash <j_k_nash@hotmail.com>
2023-03-11 08:42:16 +0000

    My first commit still working this all out

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jason Nash on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
